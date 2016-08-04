61bb5f66498f582d6aacf1980631d9b520dce9c2
iliangogov <iliangogov@gmail.com>
2016-08-04 20:48:27 +0300

    FakeEngine.cs added

--- Solution/Cosmetics.Test/Cosmetics.Engine.CosmeticsEngineTest.cs
+++ Solution/Cosmetics.Test/Cosmetics.Engine.CosmeticsEngineTest.cs
@@ -19,10 +19,10 @@ namespace Cosmetics.Test
         public void StartShouldThrowArgumentNullExceptionWhenTheInputStringOfCommandsIsNotInTheCorrectFormat()
         {
             Console.SetIn(new StringReader("fakeCommand \r\n\r\n"));
-            var mockFactory = new Mock<ICosmeticsFactory>();
-            var mockCart = new Mock<IShoppingCart>();
+            var mockedFactory = new Mock<ICosmeticsFactory>();
+            var mockedCart = new Mock<IShoppingCart>();
 
-            var engine = new CosmeticsEngine(mockFactory.Object, mockCart.Object);
+            var engine = new CosmeticsEngine(mockedFactory.Object, mockedCart.Object);
 
             Assert.Throws<ArgumentNullException>(() => engine.Start());
         }
@@ -31,19 +31,40 @@ namespace Cosmetics.Test
         //- **Start** should read, parse and execute **"CreateCategory" command**, when the passed input string is in the format that represents a CreateCategory command, which should result in adding the new Category in the list of categories.
         public void StartShouldCreateANewCategoryWhenInputIsInAValidFormat()
         {
-            Console.SetIn(new StringReader("CreateCategory ForMale\r\n\r\n"));
+            Console.SetIn(new StringReader("CreateCategory Body\r\n\r\n"));
             Console.SetOut(new StringWriter());
 
-            var mockFactory = new Mock<ICosmeticsFactory>();
-            var mockCart = new Mock<IShoppingCart>();
-            var engine = new CosmeticsEngine(mockFactory.Object, mockCart.Object);
+            var mockedFactory = new Mock<ICosmeticsFactory>();
+            var mockedCart = new Mock<IShoppingCart>();
+            var engine = new CosmeticsEngine(mockedFactory.Object, mockedCart.Object);
 
-            mockFactory.Setup(factory => factory.CreateCategory(It.IsAny<string>())).Returns(new Mock<ICategory>().Object);
+            mockedFactory.Setup(factory => factory.CreateCategory(It.IsAny<string>())).Returns(new Mock<ICategory>().Object);
 
             engine.Start();
 
-            mockFactory.Verify(mock => mock.CreateCategory(It.IsAny<string>()), Times.Once());
+            mockedFactory.Verify(factory => factory.CreateCategory(It.IsAny<string>()), Times.Once());
         }
 
+        [Test]
+        //**Start** should read, parse and execute **"AddToCategory" command**, when the passed input string is in the format that represents a AddToCategory command, which should result in adding the selected product in the respective category.   
+        public void StartShouldAddToCategoryWhenInputIsInAValidFormat()
+        {
+            Console.SetIn(new StringReader("AddToCategory Body Shampoo\r\n\r\n"));
+            Console.SetOut(new StringWriter());
+
+            var mockedFactory = new Mock<ICosmeticsFactory>();
+            var mockedCart = new Mock<IShoppingCart>();
+            var mockedCategory = new Mock<ICategory>();
+            var mockedProduct = new Mock<IProduct>();
+            var engine = new FakeEngine(mockedFactory.Object, mockedCart.Object);
+
+            mockedCategory.Setup(category => category.AddCosmetics(It.IsAny<IProduct>()));
+            engine.BaseCategories.Add("Body", mockedCategory.Object);
+            engine.BaseProducts.Add("Shampoo", mockedProduct.Object);
+            engine.Start();
+
+            mockedCategory.Verify(category => category.AddCosmetics(It.IsAny<IProduct>()), Times.Once());
+
+        }
     }
 }
---
+++ Solution/Cosmetics.Test/FakeEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetics.Contracts;
+using Cosmetics.Engine;
+
+namespace Cosmetics.Test
+{
+    internal class FakeEngine : CosmeticsEngine
+    {
+        public FakeEngine(ICosmeticsFactory factory, IShoppingCart shoppingCart) 
+            : base(factory, shoppingCart)
+        {
+        }
+        internal IDictionary<string, ICategory> BaseCategories
+        {
+            get
+            {
+                return base.categories;
+            }
+        }
+
+        internal IDictionary<string, IProduct> BaseProducts
+        {
+            get
+            {
+                return base.products;
+            }
+        }
+    }
+}
